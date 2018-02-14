0dd577794558b23290db0de0ade42a36541e398f
Timur <timur.samig@gmail.com>
2018-02-14 11:49:18 +0600

    Add project files.

---
+++ Ado.Net.6.HW2.SQLMetal/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.Net._6.HW2.SQLMetal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
